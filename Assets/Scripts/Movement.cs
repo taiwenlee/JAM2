3c9caafae1485b21d119aa32267cd2af76384674
Tai Wen Lee <leetaiwen101@gmail.com>
2022-10-28 22:35:27 -0700

    added cyotote jump and jump cache

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -22,6 +22,10 @@ public class Movement : MonoBehaviour
     public bool Modified = false;
     // Liam - Ground shake fall velocity threshold
     public float fallspeed = 21;
+    // Tai Wen - Time given for players to jump after falling off a platform
+    public float cyototeTime = 0f;
+    // Tai Wen - Time given for players to jump before touching the ground
+    public float jumpBufferTime = 0f;
 
     [Space]
     [Header("Booleans")]
@@ -39,6 +43,11 @@ public class Movement : MonoBehaviour
     private bool hasDashed;
     // Liam - variable getting max fall speed
     private float maxFall = 0;
+    // Tai Wen - counter for cyotote time
+    private float cyototeTimeCounter;
+    // Tai Wen - counter for jump buffer time
+    private float jumpBufferCounter;
+
 
     public int side = 1;
 
@@ -55,6 +64,7 @@ public class Movement : MonoBehaviour
         coll = GetComponent<Collision>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<AnimationScript>();
+        Base();
     }
 
     // Button triggers the stats to go to base version &
@@ -63,11 +73,13 @@ public class Movement : MonoBehaviour
     {
         speed = 10;
         jumpForce = 12;
-        slideSpeed = 5;
-        wallJumpLerp = 10;
-        dashSpeed = 20;
+        slideSpeed = 1;
+        wallJumpLerp = 5;
+        dashSpeed = 40;
         Modified = false;
         fallspeed = 21;
+        cyototeTime = 0f;
+        jumpBufferTime = 0f;
     }
 
     // Button triggers the stats to go to base version &
@@ -76,11 +88,13 @@ public class Movement : MonoBehaviour
     {
         speed = 10;
         jumpForce = 12;
-        slideSpeed = 5;
-        wallJumpLerp = 10;
-        dashSpeed = 20;
+        slideSpeed = 1;
+        wallJumpLerp = 5;
+        dashSpeed = 40;
         Modified = true;
         fallspeed = 21;
+        cyototeTime = 0.05f;
+        jumpBufferTime = 0.3f;
     }
 
     // Button triggers the stats to go to modified version &
@@ -89,11 +103,13 @@ public class Movement : MonoBehaviour
     {
         speed = 10;
         jumpForce = 12;
-        slideSpeed = 5;
-        wallJumpLerp = 10;
-        dashSpeed = 20;
+        slideSpeed = 1;
+        wallJumpLerp = 5;
+        dashSpeed = 40;
         Modified = true;
         fallspeed = 21;
+        cyototeTime = 0.05f;
+        jumpBufferTime = 0.3f;
     }
 
     // Update is called once per frame
@@ -108,6 +124,30 @@ public class Movement : MonoBehaviour
         Walk(dir);
         anim.SetHorizontalMovement(x, y, rb.velocity.y);
 
+        // Tai Wen - updates cyotote time counter
+        if (coll.onGround)
+        {
+            cyototeTimeCounter = cyototeTime;
+        }
+        else
+        {
+            cyototeTimeCounter -= Time.deltaTime;
+        }
+
+        // Tai Wen - updates jump buffer counter
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        // Tai Wen - resets cyotote counter if player jumps
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+            cyototeTimeCounter = -1f;
+
         // Liam - updates maxFall variable to have highest downward velocity
         if (rb.velocity.y < maxFall)
         {
@@ -116,8 +156,8 @@ public class Movement : MonoBehaviour
 
         if (coll.onWall && Input.GetButton("Fire3") && canMove)
         {
-            if(side != coll.wallSide)
-                anim.Flip(side*-1);
+            if (side != coll.wallSide)
+                anim.Flip(side * -1);
             wallGrab = true;
             wallSlide = false;
         }
@@ -133,12 +173,12 @@ public class Movement : MonoBehaviour
             wallJumped = false;
             GetComponent<BetterJumping>().enabled = true;
         }
-        
+
         if (wallGrab && !isDashing)
         {
             rb.gravityScale = 0;
-            if(x > .2f || x < -.2f)
-            rb.velocity = new Vector2(rb.velocity.x, 0);
+            if (x > .2f || x < -.2f)
+                rb.velocity = new Vector2(rb.velocity.x, 0);
 
             float speedModifier = y > 0 ? .5f : 1;
 
@@ -149,7 +189,7 @@ public class Movement : MonoBehaviour
             rb.gravityScale = 3;
         }
 
-        if(coll.onWall && !coll.onGround)
+        if (coll.onWall && !coll.onGround)
         {
             if (x != 0 && !wallGrab)
             {
@@ -163,25 +203,33 @@ public class Movement : MonoBehaviour
         if (!coll.onWall || coll.onGround)
             wallSlide = false;
 
-        if (Input.GetButtonDown("Jump"))
+        // Tai Wen - changed from checking for jump input to checking for jump buffer
+        if (jumpBufferCounter >= 0f)
         {
             anim.SetTrigger("jump");
-
-            if (coll.onGround)
+            // Tai Wen - changed from checking for ground collision to checking for cyotote counter
+            if (cyototeTimeCounter >= 0f)
+            {
                 Jump(Vector2.up, false);
+                jumpBufferCounter = -1f;   // Tai Wen - resets jump buffer counter
+            }
             // Liam - If in air, Modified enabled, and has charge of jump, do extra jump
-            if (!coll.onGround && !coll.onWall && doubleJump && Modified)
+            else if (!coll.onGround && !coll.onWall && doubleJump && Modified)
             {
                 Jump(Vector2.up, false);
                 doubleJump = false;
+                jumpBufferCounter = -1f;  // Tai Wen - resets jump buffer counter
             }
             if (coll.onWall && !coll.onGround)
+            {
                 WallJump();
+                jumpBufferCounter = -1f;  // Tai Wen - resets jump buffer counter
+            }
         }
 
         if (Input.GetButtonDown("Fire1") && !hasDashed)
         {
-            if(xRaw != 0 || yRaw != 0)
+            if (xRaw != 0 || yRaw != 0)
                 Dash(xRaw, yRaw);
         }
 
@@ -193,7 +241,7 @@ public class Movement : MonoBehaviour
             doubleJump = true;
         }
 
-        if(!coll.onGround && groundTouch)
+        if (!coll.onGround && groundTouch)
         {
             groundTouch = false;
         }
@@ -203,7 +251,7 @@ public class Movement : MonoBehaviour
         if (wallGrab || wallSlide || !canMove)
             return;
 
-        if(x > 0)
+        if (x > 0)
         {
             side = 1;
             anim.Flip(side);
@@ -300,14 +348,14 @@ public class Movement : MonoBehaviour
 
     private void WallSlide()
     {
-        if(coll.wallSide != side)
-         anim.Flip(side * -1);
+        if (coll.wallSide != side)
+            anim.Flip(side * -1);
 
         if (!canMove)
             return;
 
         bool pushingWall = false;
-        if((rb.velocity.x > 0 && coll.onRightWall) || (rb.velocity.x < 0 && coll.onLeftWall))
+        if ((rb.velocity.x > 0 && coll.onRightWall) || (rb.velocity.x < 0 && coll.onLeftWall))
         {
             pushingWall = true;
         }
